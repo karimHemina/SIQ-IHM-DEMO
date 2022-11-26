afade9ad7e825776e631818dae81d5ae21ad7447
karim.hemina <karimhemina16@gmail.com>
2022-11-26 23:13:41 +0100

    Configure the project's launcher

---
+++ src/main/java/com/karim/employees/App.java
@@ -0,0 +1,22 @@
+package com.karim.employees;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("/Views/employees.fxml"));
+        Scene scene = new Scene(parent);
+        stage.setTitle("Employees");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}

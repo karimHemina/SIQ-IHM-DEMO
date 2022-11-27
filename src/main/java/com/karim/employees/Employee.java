07eab0b22ce072120713448d302cc4897a831dd3
karim.hemina <karimhemina16@gmail.com>
2022-11-27 16:43:58 +0100

    Show list of employees from the database

--- src/main/java/com/karim/employees/Employee.java
+++ src/main/java/com/karim/employees/Employee.java
@@ -1,4 +1,50 @@
 package com.karim.employees;
 
 public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String position;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String firstName, String lastName, String position) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 }
--- src/main/java/com/karim/employees/EmployeeController.java
+++ src/main/java/com/karim/employees/EmployeeController.java
@@ -1,12 +1,30 @@
 package com.karim.employees;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class EmployeeController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class EmployeeController implements Initializable {
+
+    Connection connection = null;
+    PreparedStatement statement = null;
+    ResultSet resultSet = null;
+
+
     @FXML
     private TextField textFirstName;
 
@@ -26,6 +44,14 @@ public class EmployeeController {
     private Button btnDelete;
     @FXML
     private Button btnClear;
+    @FXML
+    private TableColumn<Employee, Integer> coldId;
+    @FXML
+    private TableColumn<Employee, String> colFirstName;
+    @FXML
+    private TableColumn<Employee, String> colLastName;
+    @FXML
+    private TableColumn<Employee, String> colPosition;
 
     @FXML
     public void createEmployee(ActionEvent actionEvent) {
@@ -41,5 +67,42 @@ public class EmployeeController {
 
     @FXML
     public void clearFields(ActionEvent actionEvent) {
+    }
+
+    public ObservableList<Employee> getEmployees(){
+        ObservableList<Employee> employees = FXCollections.observableArrayList();
+
+        String query = "select * from employee";
+        connection = Database.getConnection();
+        try {
+            statement = connection.prepareStatement(query);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String firstName = resultSet.getString("FirstName");
+                String lastName = resultSet.getString("LastName");
+                String position = resultSet.getString("Position");
+                Employee employee = new Employee(id, firstName, lastName, position);
+                employees.add(employee);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return employees;
+    }
+
+    public void showEmployees(){
+        ObservableList<Employee> employeesList = getEmployees();
+        tableEmployees.setItems(employeesList);
+        coldId.setCellValueFactory(new PropertyValueFactory<Employee, Integer>("id"));
+        colFirstName.setCellValueFactory(new PropertyValueFactory<Employee, String>("firstName"));
+        colLastName.setCellValueFactory(new PropertyValueFactory<Employee, String>("lastName"));
+        colPosition.setCellValueFactory(new PropertyValueFactory<Employee, String>("position"));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        showEmployees();
     }
 }
